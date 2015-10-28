fcbbed8bad20f2021d65f380c16cd3113352e2e5
DOTIN SCHOOL 4 <mjalalynia@gmail.com>
2015-10-28 08:18:24 +0330

    Initial commit

---
+++ src/test/MyJsonParser.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+//******************************************************************************** using json-simple 1.1, bug fixed !!!!!!!**************************************************************
+//**************** parse the json file and update the port number
+public class MyJsonParser {
+   // public static ArrayList<Deposit> depositsArray;
+    private String jsonFilePath;
+
+    public MyJsonParser(String jsonFilePath) {
+      //  this.depositsArray = new ArrayList<Deposit>();
+        this.jsonFilePath = jsonFilePath;
+    }
+
+    public void parseJson() throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(jsonFilePath));
+        Integer serverPort = (Integer) jsonObject.get("port");
+        System.out.println("\n serverPort" + serverPort);
+        JSONArray deposits = (JSONArray) jsonObject.get("deposits");
+        Iterator i = deposits.iterator();
+
+        while (i.hasNext()) {
+            JSONObject innerObj = (JSONObject) i.next();
+            Integer customer = (Integer) innerObj.get("customer");
+            Integer depositId = (Integer) innerObj.get("id");
+            Integer initialBalance = (Integer) innerObj.get("initialBalance");
+            Integer upperBound = (Integer) innerObj.get("upperBound");
+            System.out.println("\n customer" + customer
+                    + "\n depositId" + depositId
+                    + "\n initialBalance" + initialBalance
+                    + "\n upperBound" + upperBound);
+
+            Deposit deposit = new Deposit();
+            depositsArray.add(deposit);
+        }
+
+        String serverOutLog = (String) jsonObject.get("outLog");
+        System.out.println("serverOutLog" + serverOutLog);
+    }
+
+//    public void updateJson() {
+//        jsonObject.put("port", Main.portNumber.toString());
+//    }
+
+}
+
+
